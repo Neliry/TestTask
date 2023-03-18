d75a79da6b212d96392700e0fd9e1e467cbfcbab
mariyars11 <mariyars11@gmail.com>
2023-03-18 10:35:36 +0200

    Fix CustomerEntity Id type in CustomerRepository

--- src/main/kotlin/demo/repository/CustomerRepository.kt
+++ src/main/kotlin/demo/repository/CustomerRepository.kt
@@ -1,9 +1,10 @@
 package demo.repository
 
 import demo.entity.CustomerEntity
+import java.util.UUID
 import org.springframework.data.repository.CrudRepository
 
-interface CustomerRepository : CrudRepository<CustomerEntity, String> {
+interface CustomerRepository : CrudRepository<CustomerEntity, UUID> {
     fun findByEmail(email: String): CustomerEntity?
 
     fun findByPhoneNumber(phoneNumber: String): CustomerEntity?
