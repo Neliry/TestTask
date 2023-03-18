aa2aff7c805418c2908691be8d36651de5cdf540
mariyars11 <mariyars11@gmail.com>
2023-03-18 10:23:16 +0200

    Configure Swagger

--- src/main/kotlin/demo/meta/ErrorMessages.kt
+++ src/main/kotlin/demo/meta/ErrorMessages.kt
@@ -2,10 +2,10 @@ package demo.meta
 
 object ErrorMessages {
     const val PHONE_NUMBER_IS_BLANK = "Phone number must not be blank"
-    const val PHONE_NUMBER_IS_INVALID = "Phone number must be well-formed"
+    const val PHONE_NUMBER_IS_INVALID = "Phone number must be valid"
 
     const val EMAIL_IS_BLANK = "Email must not be blank"
-    const val EMAIL_IS_INVALID = "Email must be well-formed"
+    const val EMAIL_IS_INVALID = "Email must be valid"
 
     const val CUSTOMER_NOT_FOUND = "Customer not found"
     const val CUSTOMER_ALREADY_EXISTS = "Customer already exists"
